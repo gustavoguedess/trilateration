fix: convert angles from degrees to radians

ekf.py
@@ -1,7 +1,10 @@
 import numpy as np 
 
-def init_state(x, y, theta):
-    global state                    # x, y, theta
+X = 0
+Y = 1
+THETA = 2
+
+def init_state():
     global transiction_matrix       # A
     global error_covariance         # P
     global error_covariance_predict # P'
@@ -9,13 +12,19 @@ def init_state(x, y, theta):
     global measurement_noise        # R
     global observation_matrix       # H
     global identity_matrix          # I
-
-    state = np.array([x, y, theta])
+    global control_matrix           # B
 
     # Define the transiction matrix A
-    transiction_matrix = np.matrix([
+    transiction_matrix = np.array([
         [1, 0, 0],
         [0, 1, 0],
+        [0, 0, 1]
+    ])
+
+    # Define the control matrix B
+    control_matrix = np.array([
+        [0, 0, 0],
+        [0, 0, 0],
         [0, 0, 1]
     ])
 
@@ -23,14 +32,14 @@ def init_state(x, y, theta):
     error_covariance = np.identity(3)
 
     # Define the process noise Q
-    process_noise = np.matrix([
+    process_noise = np.array([
         [0.01, 0, 0],
         [0, 0.01, 0],
         [0, 0, 0.01]
     ])
 
     # Define the measurement noise R
-    measurement_noise = np.matrix([
+    measurement_noise = np.array([
         [0.1, 0, 0],
         [0, 0.1, 0],
         [0, 0, 0.1]
@@ -38,7 +47,7 @@ def init_state(x, y, theta):
 
     #TODO: calculate the observation matrix H
     # Define the observation matrix H
-    observation_matrix = np.matrix([
+    observation_matrix = np.array([
         [1, 0, 0],
         [0, 1, 0],
         [0, 0, 1]
@@ -47,38 +56,26 @@ def init_state(x, y, theta):
     # Define the identity matrix I
     identity_matrix = np.identity(3)
 
-def predict(state, state_variation):
+def predict(state, delta_state):
     global error_covariance         # P
     global error_covariance_predict # P'
     global jacobian_matrix          # A
-    
-    # Convert the states to numpy arrays
-    state = np.matrix([state['x'], state['y'], state['theta']]).T
-    state_variation = np.matrix([state_variation[0], state_variation[1], state_variation[2]]).T
-
-    # Extract the values for the jacobian matrix
-    theta = state[2, 0] # theta
-    delta_x = state_variation[0, 0] # delta_x
-    delta_y = state_variation[1, 0] # delta_y
 
     # calc the jacobian matrix A
-    jacobian_matrix = np.matrix([
-        [1, 0, -delta_x*np.sin(theta)+delta_y*np.cos(theta)],
-        [0, 1, delta_x*np.cos(theta)-delta_y*np.sin(theta)],
+    jacobian_matrix = np.array([
+        [1, 0, -delta_state[X]*np.sin(state[THETA])+delta_state[Y]*np.cos(state[THETA])],
+        [0, 1, delta_state[X]*np.cos(state[THETA])-delta_state[Y]*np.sin(state[THETA])],
         [0, 0, 1]
     ])
-
-    #TODO: Check if this is correct.    
-    # Predict the next state. what is B*u?
+       
+    # Predict the next state
     # x' = A*x + B*u
-    predict_state = jacobian_matrix @ state #+ B*u?+
+    predict_state = jacobian_matrix @ state + control_matrix @ delta_state
 
     # Predict the next error covariance
     # P' = A*P*A.T + Q
     error_covariance_predict = transiction_matrix @ error_covariance @ transiction_matrix.T + process_noise
 
-
-    predict_state = {'x': predict_state[0, 0], 'y': predict_state[1, 0], 'theta': predict_state[2, 0]}
     return predict_state
 
 def correct(predict_state, tril_state):
@@ -88,8 +85,8 @@ def correct(predict_state, tril_state):
     global identity_matrix          # I
 
     # Convert the states to numpy arrays
-    predict_state = np.matrix([predict_state['x'], predict_state['y'], predict_state['theta']]).T
-    tril_state = np.matrix([tril_state['x'], tril_state['y'], tril_state['theta']]).T
+    predict_state = np.array([predict_state['x'], predict_state['y'], predict_state['theta']]).T
+    tril_state = np.array([tril_state['x'], tril_state['y'], tril_state['theta']]).T
     
     # Calculate the Kalman Gain K
     # K = P' * H.T * (H * P' * H.T + R)^-1

functions/simtwo.py
@@ -1,5 +1,6 @@
 from functions.client_udp import ClientUDP
 import json
+import numpy as np
 
 class SimTwo:
     def __init__(self):
@@ -24,7 +25,15 @@ class SimTwo:
             'raw_lidar': data
         }
         return data
-
+    
+    def state2array(self, data):
+        x, y, theta = data['x'], data['y'], data['theta']*np.pi/180
+        return np.array([x, y, theta]).T
+    
+    def encoders2array(self, data):
+        u1, u2, u3, u4 = data['u1'], data['u2'], data['u3'], data['u4']
+        return np.array([u1, u2, u3, u4]).T
+    
     def get_lidar_data(self):
         msg = self.client.recv()
         data = self.convert_msg(msg)

main.py
@@ -3,20 +3,22 @@ import trilateration as tril
 import time
 import odometry
 import ekf
+import numpy as np
+
+X = 0
+Y = 1
+THETA = 2
 
 st = SimTwo()
 
 def get_initial_trilateration():
     data = st.get_data()
 
-    x, y, theta, u1, u2, u3, u4 = data['ground_truth'].values()
-    trilateration = {
-        'x': x,
-        'y': y,
-        'theta': theta,
-    }
+    state = st.state2array(data['ground_truth'])
+    encoders = st.encoders2array(data['ground_truth'])
 
-    return trilateration
+    return state
+
 
 time_last_iteration = time.time()
 def get_diff_time():
@@ -29,37 +31,37 @@ def get_diff_time():
 
 def main():
     state = get_initial_trilateration()
-    ekf.init_state(state['x'], state['y'], state['theta'])
-    last_gt = st.get_data()['ground_truth']
-    last_gt = [last_gt['x'], last_gt['y'], last_gt['theta']]
-
-    print_state(state, 'First ')
-    print()
+    last_gt = state.copy()
+    ekf.init_state()
 
     acm_time = 0
     while input_data := st.get_data():
         time_diff = get_diff_time()
         acm_time+=time_diff
 
-        velocity_amr = odometry.calc_amr_velocity(
-            enc1 = input_data['ground_truth']['u1'],
-            enc2 = input_data['ground_truth']['u2'],
-            enc3 = input_data['ground_truth']['u3'],
-            enc4 = input_data['ground_truth']['u4'],
-        )
-        velocity = odometry.calc_velocity(velocity_amr, input_data['ground_truth']['theta'])
+        # Get data
+        data_lidar = input_data['raw_lidar']
+        ground_truth = st.state2array(input_data['ground_truth'])
+        encoders = st.encoders2array(input_data['ground_truth'])
 
-        gt = [input_data['ground_truth']['x'], input_data['ground_truth']['y'], input_data['ground_truth']['theta']]
-        gt_velocity = odometry.calc_velocity_ground_truth(last_gt, gt, time_diff)
-        last_gt = gt
+        # Calculate relative velocity of AMR
+        velocity_amr = odometry.calc_amr_velocity(encoders = encoders)
+
+        # Calculate absolute velocity of AMR
+        velocity = odometry.calc_velocity(velocity_amr, ground_truth[THETA])
+
+        # Calculate ground truth velocity
+        gt_velocity = odometry.calc_velocity_ground_truth(last_gt, ground_truth, time_diff)
+        last_gt = ground_truth
+        
+        # Trilateration
+        tril_state = tril.calc_trilateration(data_lidar, last_state=state)
 
         # EKF Predict
         predict_state = ekf.predict(
             state = state,
-            state_variation = velocity*time_diff,
+            delta_state = velocity*time_diff,
         )
-
-        tril_state = tril.calc_trilateration(input_data['raw_lidar'], last_trilateration=state)
 
         # if acm_time > 0.05:
         #     state = ekf.correct (
@@ -72,34 +74,40 @@ def main():
 
         # Print data to DEBUG
         print_time_diff(time_diff)
-        print_state_error(state, input_data['ground_truth'], 'Trilateration')
         print_encoders(input_data['ground_truth'])
         print_velocity(velocity_amr, 'AMR')
-        print_velocity(velocity, 'Abs', ground_truth=gt_velocity)
-
+        print_velocity(velocity, 'Abs')
+        print_velocity(gt_velocity, 'GT ')
+        print_velocity_error(velocity, gt_velocity)
+        print()
+        
+        print_state(tril_state, 'Trilateration')
+        print_state(predict_state, 'Predicted')
+        print_state(ground_truth, 'Ground truth')
+        print_state_error(state, ground_truth)
         print()
 
-        print_state(input_data['ground_truth'], ' Ground truth')
+        print_state(ground_truth, ' Ground truth')
         print_state(predict_state, '    Predicted')
         print_state(state, 'Trilateration')
 
-        print_clear_last_line(n=9)
+        print_clear_last_line(n=20)
 
 
 
-def print_state(State, name=''):
-    x = State['x']
-    y = State['y']
-    theta = State['theta']
+def print_state(state, name=''):
+    x = state[X]
+    y = state[Y]
+    theta = state[THETA]
 
     if name:
         print(f'{name} ', end='')
     print(f'State: x={x:.4f}, y={y:.4f}, theta={theta:.2f}')
 
 def print_state_error(state, ground_truth, name=''):
-    x_error = abs(state['x'] - ground_truth['x'])
-    y_error = abs(state['y'] - ground_truth['y'])
-    theta_error = abs(state['theta'] - ground_truth['theta'])
+    x_error = abs(state[X] - ground_truth[X])
+    y_error = abs(state[Y] - ground_truth[Y])
+    theta_error = abs(state[THETA] - ground_truth[THETA])
 
     if name:
         print(f'{name} ', end='')
@@ -110,14 +118,14 @@ def print_velocity(velocity, name='', ground_truth=None):
 
     if name:
         print(f'{name} ', end='')
-    print(f'Velocity: x={x: .4f}, y={y: .4f}, theta={theta: .2f}', end='')
+    print(f'Velocity: x={x: .4f}, y={y: .4f}, theta={theta: .2f}')
 
-    if ground_truth:
-        x_error = abs(velocity[0] - ground_truth[0])
-        y_error = abs(velocity[1] - ground_truth[1])
-        theta_error = abs(velocity[2] - ground_truth[2])
-        print(f'    Error: x={x_error:.4f}, y={y_error:.4f}, theta={theta_error:.2f}', end='')
-    print()
+def print_velocity_error(velocity, ground_truth):
+    x_error = abs(velocity[0] - ground_truth[0])
+    y_error = abs(velocity[1] - ground_truth[1])
+    theta_error = (velocity[2] - ground_truth[2] + np.pi) % (2*np.pi) - np.pi
+    degree = np.rad2deg(theta_error)
+    print(f'Velocity Error: x={x_error: .4f}, y={y_error: .4f}, theta={theta_error: .2f} ({degree: .2f}°)')
 
 def print_encoders(ground_truth):
     u1 = ground_truth['u1']
@@ -129,6 +137,9 @@ def print_encoders(ground_truth):
 
 def print_time_diff(time_diff):
     print(f'Time diff: {time_diff:.4f}s')
+
+def print_clear_screen():
+    print('\033[2J', end='')
 
 def print_clear_last_line(n=1):
     for _ in range(n):

odometry.py
@@ -2,25 +2,21 @@ import numpy as np
 import amr_params as amr
 
 def calc_velocity_ground_truth(last_gt, gt, time_diff):
-    if time_diff == 0:
-        return [0, 0, 0]
-    velocity = [
+    velocity = np.array([
         (gt[0] - last_gt[0])/time_diff,
         (gt[1] - last_gt[1])/time_diff,
         (gt[2] - last_gt[2])/time_diff,
-    ]
+    ]).T
 
     return velocity
 
-def calc_amr_velocity(enc1, enc2, enc3, enc4):
+def calc_amr_velocity(encoders):
     l = amr.WHEELS_LENGTH
     w = amr.WHEELS_WIDTH
     r = amr.WHEELS_RADIUS
-    
-    enc = np.array([enc1, enc2, enc3, enc4]).T
 
     time_simtwo = 1/25 # 25 Hz
-    u = enc*2*np.pi/(30*64*2*time_simtwo) # 30 is the gear ratio, 64 is the encoder resolution. 2 is a magical number that makes it work
+    u = encoders*2*np.pi/(30*64*2*time_simtwo) # 30 is the gear ratio, 64 is the encoder resolution. 2 is a magical number that makes it work
 
     ########################################
     H = 1/r * np.array([
@@ -31,16 +27,16 @@ def calc_amr_velocity(enc1, enc2, enc3, enc4):
     ])
 
     H_pseudoinverse = np.linalg.pinv(H)
-    # velocity = H_pseudoinverse @ u
+    velocity = H_pseudoinverse @ u
     
     ####### Alternative calculation #######
-    value = 1/(l+w)
-    H_pseudoinverse_alternative = r/4 * np.array([
-        [1, 1, 1, 1],
-        [-1, 1, -1, 1],
-        [-value, value, value, -value],
-    ])
-    velocity = H_pseudoinverse_alternative @ u
+    # value = 1/(l+w)
+    # H_pseudoinverse_alternative = r/4 * np.array([
+    #     [1, 1, 1, 1],
+    #     [-1, 1, -1, 1],
+    #     [-value, value, value, -value],
+    # ])
+    # velocity = H_pseudoinverse_alternative @ u
 
     return velocity 
 
@@ -56,7 +52,7 @@ def calc_velocity(velocity_amr, theta):
         velocity_amr[0]*np.cos(theta) - velocity_amr[1]*np.sin(theta),
         velocity_amr[0]*np.sin(theta) + velocity_amr[1]*np.cos(theta),
         velocity_amr[2],
-    ])
+    ]).T
 
     return {
         'x': velocity_amr['x']*np.cos(theta) - velocity_amr['y']*np.sin(theta),

trilateration.py
@@ -11,6 +11,9 @@ LIMIT_GAP = 3 # Number of samples between two objects to consider them as the sa
 
 BEACON_RADIUS = 0.055
 
+X = 0
+Y = 1
+THETA = 2
 
 def calc_trilateration(data, last_state):
     # Remove outliers
@@ -28,6 +31,8 @@ def calc_trilateration(data, last_state):
     # Get position and angle from beacons
     x,y = get_position(beacons)
     theta = calc_theta(beacons)
+
+    return np.array([x, y, theta]).T
 
     trilateration = {
         'x': x,
@@ -109,8 +114,8 @@ def get_distance_group(data, start, end):
 
 
 ##### GETTING BEACONS #####
-def get_beacons(objs, last_trilateration):
-    last_beacons = calc_beacons(last_trilateration)
+def get_beacons(objs, last_state):
+    last_beacons = calc_beacons(last_state)
 
     beacons = {
         'beacon1': get_similar_beacon(objs, last_beacons['beacon1']),
@@ -126,30 +131,33 @@ def get_beacons(objs, last_trilateration):
 
     return beacons
 
-def calc_beacons(trilateration):
+def calc_beacons(state):
     beacons = {
-        'beacon1': calc_beacon(trilateration, amr.WHEELS_LEFT_X, amr.WHEELS_UPPER_Y),
-        'beacon2': calc_beacon(trilateration, amr.WHEELS_LEFT_X, amr.WHEELS_BOTTOM_Y),
-        'beacon3': calc_beacon(trilateration, amr.WHEELS_RIGHT_X, amr.WHEELS_BOTTOM_Y),
-        'beacon4': calc_beacon(trilateration, amr.WHEELS_RIGHT_X, amr.WHEELS_UPPER_Y),
+        'beacon1': calc_beacon(state, amr.WHEELS_LEFT_X, amr.WHEELS_UPPER_Y),
+        'beacon2': calc_beacon(state, amr.WHEELS_LEFT_X, amr.WHEELS_BOTTOM_Y),
+        'beacon3': calc_beacon(state, amr.WHEELS_RIGHT_X, amr.WHEELS_BOTTOM_Y),
+        'beacon4': calc_beacon(state, amr.WHEELS_RIGHT_X, amr.WHEELS_UPPER_Y),
     }
     return beacons
 
-def calc_beacon(trilateration, bx, by):
+def calc_beacon(state, bx, by):
     return {
         'x': bx,
         'y': by,
-        'phi': np.degrees(calc_phi(trilateration, bx, by)),
-        'distance': euclidean_distance(trilateration['x'], trilateration['y'], bx, by),
+        'phi': calc_phi(state, bx, by),
+        'distance': euclidean_distance(state[X], state[Y], bx, by),
     }
 
 # Calculate the phi angle in radians between the trilateration and the beacon. The angle is between 0 and 2pi
 def calc_phi(trilateration, bx, by):
-    dy = by - trilateration['y']
-    dx = bx - trilateration['x']
-    gamma = math.atan2(dy, dx)
-    theta = trilateration['theta'] if trilateration['theta'] >= 0 else 360 + trilateration['theta']
-    theta = math.radians(theta)
+    dy = by - trilateration[Y]
+    dx = bx - trilateration[X]
+    theta = trilateration[THETA]
+    
+    gamma = np.arctan2(dy, dx)
+    # theta = trilateration['theta'] if trilateration['theta'] >= 0 else 360 + trilateration['theta']
+    # theta = math.radians(theta)
+
     phi = diff_angle(gamma, theta)
     phi = (phi + 2 * np.pi) % (2 * np.pi)
     return phi
@@ -157,8 +165,12 @@ def calc_phi(trilateration, bx, by):
 
 # Return the difference between two angles in radians (-pi, pi)
 def diff_angle(angle1, angle2):
-    diff = (angle1 - angle2 + 2 * np.pi) % (2 * np.pi)
-    if diff > np.pi:
+    diff = angle1 - angle2
+    
+    # Normalize the difference
+    if diff < -np.pi:
+        diff += 2 * np.pi
+    elif diff > np.pi:
         diff -= 2 * np.pi
     return diff
 
@@ -202,7 +214,7 @@ def error_distance_beacon(distance):
 
 #TODO: Improve this function
 def error_phi_beacon(phi):
-    return 15
+    return np.radians(15) # 15 degrees in radians of error
 
 def obj_to_beacon(obj, last_beacon):
     phi = np.degrees(circular_mean([np.radians(obj['start']), np.radians(obj['end'])]))
@@ -216,7 +228,7 @@ def obj_to_beacon(obj, last_beacon):
     return {
         'x': last_beacon['x'],
         'y': last_beacon['y'],
-        'phi': phi,
+        'phi': phi_rad, # in radians
         'distance': distance,
     }
 
@@ -287,17 +299,17 @@ def calc_theta(beacons):
     dist_4 = beacons['beacon4']['distance'] if 'distance' in beacons['beacon4'] else 0
 
     # phi
-    phi_1 = np.radians(beacons['beacon1']['phi']) if 'phi' in beacons['beacon1'] else 0
-    phi_2 = np.radians(beacons['beacon2']['phi']) if 'phi' in beacons['beacon2'] else 0
-    phi_3 = np.radians(beacons['beacon3']['phi']) if 'phi' in beacons['beacon3'] else 0
-    phi_4 = np.radians(beacons['beacon4']['phi']) if 'phi' in beacons['beacon4'] else 0
+    phi_1 = beacons['beacon1']['phi'] if 'phi' in beacons['beacon1'] else 0
+    phi_2 = beacons['beacon2']['phi'] if 'phi' in beacons['beacon2'] else 0
+    phi_3 = beacons['beacon3']['phi'] if 'phi' in beacons['beacon3'] else 0
+    phi_4 = beacons['beacon4']['phi'] if 'phi' in beacons['beacon4'] else 0
 
     # diff phi between beacons
     delta_12 = diff_angle(phi_2, phi_1)
     delta_23 = diff_angle(phi_3, phi_2)
     delta_34 = diff_angle(phi_4, phi_3)
     delta_41 = diff_angle(phi_1, phi_4)
-    
+
     # gamma
     b1_gamma   =  (np.pi/2   + asin( np.sin(delta_12) * dist_2 / dist_12 ) ) % (2*np.pi)
     b2_gamma   =  (np.pi     + asin( np.sin(delta_23) * dist_3 / dist_23 ) ) % (2*np.pi)
@@ -320,14 +332,13 @@ def calc_theta(beacons):
     
     theta_list = [b1_theta, b2_theta, b3_theta, b4_theta, b1_theta_l, b2_theta_l, b3_theta_l, b4_theta_l]
 
-    theta_rad = circular_mean(theta_list)
+    theta = circular_mean(theta_list)
 
-    theta_degree = np.degrees(theta_rad)
+    # Normalize to [-np.pi, np.pi]
+    theta = (theta + 2 * np.pi) % (2 * np.pi)
+    if theta > np.pi:
+        theta = theta - 2 * np.pi
 
-    theta = theta_degree if theta_degree < 180 else theta_degree - 360
-
-    if theta is None: 
-        return None
     return theta
 
 